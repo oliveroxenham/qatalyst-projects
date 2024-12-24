98f54be39d44851b411b263f0c4686de951be636
Oliver Oxenham <oliver.oxenham@gmail.com>
2024-12-24 08:34:56 +0400

    authentication with Clerk

--- app/(authenticated)/projects/page.tsx
+++ app/(authenticated)/projects/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { useClerk } from '@clerk/nextjs';
+
 export default function Projects() {
+  const { signOut } = useClerk();
   return (
     <div>
       <p>Projects</p>
       <Link href="/projects/Io7hJkUHqEcSdZknSIUT-/scorecard2">Scorecard</Link>
-      {/* <Button onClick={() => signOut()}>Sign Out</Button> */}
+      <Button onClick={() => signOut()}>Sign Out</Button>
     </div>
   );
 }
