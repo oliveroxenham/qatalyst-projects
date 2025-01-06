9a2bc2fb648fdb2d5d025900c1316df6d4234538
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-01-06 15:28:11 +0400

    sign in video

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,16 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black flex justify-center items-center">
+      <video
+        src="https://v3jxx0dboaeguwsf.public.blob.vercel-storage.com/globe-trim-xZC2QMW0T9HfyvTBcAqzXq9WChcEwI.mp4"
+        autoPlay
+        loop
+        muted
+        className="absolute w-auto min-w-full min-h-full max-w-none"
+      ></video>
+      <SignIn />
+    </div>
+  );
 }
