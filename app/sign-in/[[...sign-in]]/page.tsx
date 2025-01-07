427379811fd4265fb8071bad2ead374c9484f1e7
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-01-07 08:47:35 +0400

    sign in background gradient

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,14 +2,15 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black flex justify-center items-center">
+    <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black">
       <video
         src="https://v3jxx0dboaeguwsf.public.blob.vercel-storage.com/globe-trim-xZC2QMW0T9HfyvTBcAqzXq9WChcEwI.mp4"
         autoPlay
         loop
         muted
         className="absolute w-auto min-w-full min-h-full max-w-none"
-      ></video>
+      />
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[radial-gradient(#2A9D90,#000000)] opacity-65" />
       <SignIn />
     </div>
   );
