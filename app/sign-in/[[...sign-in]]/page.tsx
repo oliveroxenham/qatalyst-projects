3276cffd2901267c8558b464d729ee8f3bcc5825
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-01-07 08:50:43 +0400

    remove overflow in signin page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black">
+    <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black overflow-hidden">
       <video
         src="https://v3jxx0dboaeguwsf.public.blob.vercel-storage.com/globe-trim-xZC2QMW0T9HfyvTBcAqzXq9WChcEwI.mp4"
         autoPlay
