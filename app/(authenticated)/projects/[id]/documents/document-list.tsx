a6d087d89c737b53325231ab5a41fa7022ce3675
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-01-10 15:39:49 +0400

    document viewer

--- app/(authenticated)/projects/[id]/documents/document-list.tsx
+++ app/(authenticated)/projects/[id]/documents/document-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import {
   Table,
   TableBody,
@@ -13,49 +14,64 @@ import { File } from 'lucide-react';
 import type { Document } from '@/types/document';
 import { getDocumentsClient } from '@/server/db';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import DocumentViewer from './document-viewer';
 
 export default function DocumentList() {
+  const [documentUrl, setDocumentUrl] = useState<string>();
   const { data: documentList } = useSuspenseQuery<Document[]>({
     queryKey: ['documents'],
     queryFn: getDocumentsClient,
-  }); 
-   
+  });
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="flex gap-1 items-center">
-            <File />
-            Filename
-          </TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Size</TableHead>
-          <TableHead>Source</TableHead>
-          <TableHead>Date</TableHead>
-          <TableHead>Last activity</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {documentList?.map((document) => (
-          <TableRow key={document.id}>
-            <TableCell className="flex items-center gap-1">
-              <Image
-                src={`/icons/${document.type}.svg`}
-                alt={document.type}
-                className="w-7 h-7"
-                width={28}
-                height={28}
-              />
-              {document.name}
-            </TableCell>
-            <TableCell>{document.status}</TableCell>
-            <TableCell>{document.size}</TableCell>
-            <TableCell>{document.source}</TableCell>
-            <TableCell>{document.date}</TableCell>
-            <TableCell>{document.lastActivity}</TableCell>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="flex gap-1 items-center">
+              <File />
+              Filename
+            </TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Size</TableHead>
+            <TableHead>Source</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Last activity</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {documentList?.map((document) => (
+            <TableRow
+              className="hover:cursor-pointer"
+              key={document.id}
+              onClick={() =>
+                setDocumentUrl(
+                  documentUrl
+                    ? undefined
+                    : 'https://v3jxx0dboaeguwsf.public.blob.vercel-storage.com/PROJ_DESC_1650_29DEC2014-VyhmiOgjaRAEF7vDr2tPD435gFrLw1.pdf'
+                )
+              }
+            >
+              <TableCell className="flex items-center gap-1">
+                <Image
+                  src={`/icons/${document.type}.svg`}
+                  alt={document.type}
+                  className="w-7 h-7"
+                  width={28}
+                  height={28}
+                />
+                {document.name}
+              </TableCell>
+              <TableCell>{document.status}</TableCell>
+              <TableCell>{document.size}</TableCell>
+              <TableCell>{document.source}</TableCell>
+              <TableCell>{document.date}</TableCell>
+              <TableCell>{document.lastActivity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <DocumentViewer documentUrl={documentUrl} setDocumentUrl={setDocumentUrl} />
+    </>
   );
 }
---
+++ app/(authenticated)/projects/[id]/documents/document-viewer.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { Button } from '@/components/qbutton';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarProvider,
+} from '@/components/ui/sidebar';
+import { X } from 'lucide-react';
+
+export default function DocumentViewer({
+  documentUrl,
+  setDocumentUrl,
+}: {
+  documentUrl?: string;
+  setDocumentUrl: (url?: string) => void;
+}) {
+  return (
+    <SidebarProvider
+      open={documentUrl ? true : false}
+      style={{
+        // @ts-ignore
+        '--sidebar-width': '50%',
+        '--sidebar-mobile-width:': '100%',
+      }}
+    >
+      <Sidebar side="right">
+        <SidebarHeader className="bg-neutral-100 h-[88px] flex flex-row items-center justify-between px-4">
+          <span className="text-lg text-foreground font-bold">
+            1650 Keo Seima REDD Project
+          </span>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => setDocumentUrl(undefined)}
+          >
+            <X />
+          </Button>
+        </SidebarHeader>
+        <SidebarContent></SidebarContent>
+      </Sidebar>
+    </SidebarProvider>
+  );
+}
