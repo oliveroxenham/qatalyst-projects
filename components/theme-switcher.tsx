7a21c2b5d1891e7960809c45a7e8f393e5b6318e
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-01-23 15:32:50 +0400

    fixes

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -1,29 +1,28 @@
 'use client';
 
-// import { useState, useEffect } from 'react';
-// import { useTheme } from 'next-themes';
-// import { Button } from '@/components/qbutton';
-// import { Moon, Sun, SunMoon } from 'lucide-react';
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/qbutton';
+import { Moon, Sun, SunMoon } from 'lucide-react';
 
 export function ThemeSwitcher() {
-  return <div />;
-  // const { theme, setTheme } = useTheme();
-  // const [isMounted, setIsMounted] = useState(false);
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
-  // const toggleTheme = () => {
-  //   if (theme === 'light') {
-  //     setTheme('dark');
-  //   } else {
-  //     setTheme('light');
-  //   }
-  // };
-  // return isMounted ? (
-  //   <Button onClick={() => toggleTheme()} variant="ghost" size="sm">
-  //     {theme === 'light' && <Sun />}
-  //     {theme === 'dark' && <Moon />}
-  //     {theme === 'system' && <SunMoon />}
-  //   </Button>
-  // ) : null;
+  const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  return isMounted ? (
+    <Button onClick={() => toggleTheme()} variant="ghost" size="sm">
+      {theme === 'light' && <Sun />}
+      {theme === 'dark' && <Moon />}
+      {theme === 'system' && <SunMoon />}
+    </Button>
+  ) : null;
 }
