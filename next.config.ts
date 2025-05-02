1c508e26ba76e5d609781d30c3193b84db1b50c4
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-05-02 14:38:28 +0400

    redirect

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  // // Add env variables that should be accessible to the client
-  // env: {
-  //   NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'https://qatalyst-demo.vercel.app',
-  //   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  // },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: '/projects',
+        permanent: true,
+      },
+    ]
+  },
   // Server-side env variables are automatically available in Server Components and API Routes
   webpack(config) {
     config.module.rules.push({
